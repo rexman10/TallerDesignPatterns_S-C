134547d5e9ede6aa623f694521770332da6f6914
DFlamis <derabast@espol.edu.ec>
2022-07-14 17:01:47 -0500

    Agregar el main

--- Decorator/Main.java
+++ Decorator/Main.java
@@ -1,9 +1,24 @@
-import Implements.NotificationBase;
 
 public class Main
 {
     public static void main(String[] args)
     {
-        // Notificaio
+        Client karen = new Client("Karen");
+        karen.useWhatsapp();
+
+        Client fabricsio = new Client("Fabricsio");
+        fabricsio.useWhatsapp();
+        fabricsio.useTelegram();
+
+        Client zaida = new Client("Zaida");
+        zaida.useWhatsapp();
+        zaida.useSignal();
+        zaida.useWire();
+
+        System.out.println( karen.getNotifications() );
+        System.out.println( fabricsio.getNotifications() );
+        System.out.println( zaida.getNotifications() );
+
+        
     }    
 }
